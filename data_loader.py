b420e69f06ac69f5333f2666512e79f877a45b09
LujainBakkar <lujain.bkar@gmail.com>
2026-06-08 16:06:59 +0300

    Remove file logging handler from data_loader.py
    Removed file handler for logging.

--- data_loader.py
+++ data_loader.py
@@ -18,7 +18,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('energy_analysis.log'),
         logging.StreamHandler()
     ]
 )
